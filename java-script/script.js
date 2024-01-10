41ce9d2dfff9106a80bdb8405280b9c672c933c4
Naveen <naveenreddy.can@gmail.com>
2024-01-10 12:39:18 -0500

    edited a lot styles

--- java-script/script.js
+++ java-script/script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const hamburger = document.querySelector('.hamburger');
-  const leftWing = document.querySelector('.left-wing');
 
-  hamburger.addEventListener('click', function() {
-    leftWing.classList.toggle('show');
-  });
-});
